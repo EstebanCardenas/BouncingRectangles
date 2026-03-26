4af7074ea6ed6adf9444b0eff8002c22fa1517d4
Nicolás Cárdenas <ne.cardenas@uniandes.edu.co>
2026-03-26 01:56:35 -0500

    Remove window obj from window config

--- src/config/engine_config.py
+++ src/config/engine_config.py
@@ -9,13 +9,10 @@ class EngineConfig:
     def load_config(self, config_path: str):
         with open(config_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        config = data['window']
-        if config == None:
-            raise Exception('Config has no window object')
-        self.title = config['title']
-        self.size = (config['size']['w'], config['size']['h'])
-        bg_color = config['bg_color']
+        self.title = data['title']
+        self.size = (data['size']['w'], data['size']['h'])
+        bg_color = data['bg_color']
         self.bg_color = (
             bg_color['r'], bg_color['g'], bg_color['b']
         )
-        self.framerate = config['framerate']
+        self.framerate = data['framerate']
